d4128ccd6e78b1a578424f4536e49ecc2ecd4612
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2022-07-31 17:08:12 -0500

    Find stable training config

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ config = {
     'device' : {
         'home directory' : './',
         'initial seed': 1, 
-        'device type': 'mps', 
+        'device type': 'cpu', 
         
         }, 
     'data' : {
-        'batch size': 128,
+        'batch size': 32,
         'num workers': 8, 
         'image shape': (1, 28, 28),
         'num classes': 10, 
@@ -14,14 +14,14 @@ config = {
         'test samples': 4000
         }, 
     'generator' : {
-         'generator blocks' : 3, 
+         'generator blocks' : 2, 
     }, 
     'discriminator' : {
         'discriminator blocks' : 3, 
     }, 
     'hyperparameters' : {
-        'latent dimension': 256,
-        'learning rate': 0.0001, 
+        'latent dimension': 100,
+        'learning rate': 0.0002, 
         'beta1': 0.5,
         'beta2': 0.999,
         'epochs': 200, 
